4c81429ce87f76205b7a0ec4af2c666ea6ca8315
bgrochal <bartek.grochal@gmail.com>
2017-11-30 23:53:52 +0100

    Integration with the Configuration Server.

--- src/main/scala/pl/edu/agh/iet/dts/processing/ProcessingService.scala
+++ src/main/scala/pl/edu/agh/iet/dts/processing/ProcessingService.scala
@@ -1,10 +1,14 @@
 package pl.edu.agh.iet.dts.processing
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 /**
   * @author Bartłomiej Grochal
   */
+@SpringBootApplication
 class ProcessingService
 
 object ProcessingService extends App {
-  println("Hello!")
+  SpringApplication.run(classOf[ProcessingService], args: _*)
 }
