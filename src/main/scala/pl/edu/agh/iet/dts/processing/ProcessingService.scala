032cb4132a6d00c974bdad42b20454b88780d800
bgrochal <bartek.grochal@gmail.com>
2017-12-01 17:48:08 +0100

    Integration with the Eureka service discovery.

--- src/main/scala/pl/edu/agh/iet/dts/processing/ProcessingService.scala
+++ src/main/scala/pl/edu/agh/iet/dts/processing/ProcessingService.scala
@@ -2,11 +2,13 @@ package pl.edu.agh.iet.dts.processing
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 /**
   * @author Bartłomiej Grochal
   */
 @SpringBootApplication
+@EnableEurekaClient
 class ProcessingService
 
 object ProcessingService extends App {
