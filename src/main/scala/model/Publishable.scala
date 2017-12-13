d81e0e62a5b87e597bebf27db8877de975fd7524
bgrochal <bartek.grochal@gmail.com>
2017-12-13 19:37:46 +0100

    Coordinates JSON field names changed.

--- src/main/scala/model/Publishable.scala
+++ src/main/scala/model/Publishable.scala
@@ -3,7 +3,7 @@ package model
 
 sealed trait Publishable
 
-case class Coordinate(lat: Double, long: Double)
+case class Coordinate(latitude: Double, longitude: Double)
 
 //Newest event in batch timestamp
 case class AggregatedResult(ownerId: String, data: List[Coordinate], timestamp: Long) extends Publishable
